de1c5e5727d2e87a94e06612a9cd0893c9db0044
FereWulf <finn@toxicflames.co.uk>
2025-01-23 23:11:46 +0000

    Redid walls + gate models & merged fort and watchtower

--- Source/Diplosim/Buildings/Work/Defence/Fort.cpp
+++ Source/Diplosim/Buildings/Work/Defence/Fort.cpp
@@ -13,14 +13,22 @@ void AFort::Enter(ACitizen* Citizen)
 {
 	Super::Enter(Citizen);
 
-	if (GetOccupied().Contains(Citizen))
-		Citizen->AttackComponent->RangeComponent->SetSphereRadius(800.0f);
+	if (!GetOccupied().Contains(Citizen))
+		return;
+
+	int32 z = FMath::Clamp(BuildingMesh->GetStaticMesh()->GetBounds().GetBox().GetSize().Z / 100.0f, 1, 5);
+
+	Citizen->AttackComponent->RangeComponent->SetSphereRadius(Citizen->AttackComponent->RangeComponent->GetUnscaledSphereRadius() * z);
 }
 
 void AFort::Leave(ACitizen* Citizen)
 {
 	Super::Leave(Citizen);
 
-	if (GetOccupied().Contains(Citizen))
-		Citizen->AttackComponent->RangeComponent->SetSphereRadius(400.0f);
+	if (!GetOccupied().Contains(Citizen))
+		return;
+
+	int32 z = FMath::Clamp(BuildingMesh->GetStaticMesh()->GetBounds().GetBox().GetSize().Z / 100.0f, 1, 5);
+
+	Citizen->AttackComponent->RangeComponent->SetSphereRadius(Citizen->AttackComponent->RangeComponent->GetUnscaledSphereRadius() / z);
 }
